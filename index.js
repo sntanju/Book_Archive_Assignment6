a21cbe69cbe91a5a8c3cd2981eb49e3b6d230228
sntanju <sntanju07@gmail.com>
2021-09-02 18:22:50 +0600

    dinamic-bookAmount-load

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const searchBook = () => {
 
 // Send Error Message For Empty Input Field
 if(searchField.value === '') {
-   emptyField.innerText = 'Please write a name of book';
+   emptyField.innerText = 'Please Write A Name Of Book';
    document.getElementById('search-result').innerText = '';
 }
  
@@ -39,9 +39,16 @@ const displaySearchResult = books => {
 
 // Clear previous Result
     searchResult.textContent = '';
-    
+    let showBook = 0;
+    if(books.length <= 30 ) showBook = books.length;
+    else showBook = 30;
+    const result = `Showing ${showBook} Book From Search Result ${books.length} Book`;
+    emptyField.innerText = result;
+    let resultCount = 0;
+
     books.forEach(book => {
-        //console.log(book);
+        resultCount++;
+        if( resultCount <= 30){
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -49,14 +56,15 @@ const displaySearchResult = books => {
             <img src=" https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h3 class="card-title text-primary mb-3">${book?.title}</h3>
-              <h4 class="card-text text-success mb-3">${book?.author_name[0]}</h4>
-              <h5 class="card-text text-success">${book?.publisher[0]}</h5>
+              <h4 class="card-text text-success mb-3">${book?.author_name}</h4>
+              <h5 class="card-text text-success">${book?.publisher}</h5>
               <h5 class="card-text ">${book?.first_publish_year}</h5>
             </div>
           </div>
         `;
         
         searchResult.appendChild(div);
-        })
+        }
+         })
     }
 }
